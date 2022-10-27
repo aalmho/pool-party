81ef6090188857e033028d79b341d934d16edeb3
Michael <michael@MacBook-Pro-tilhrende-Michael.local>
2022-10-27 22:09:00 +0200

    add authentication component

--- components/Authentication.tsx
+++ components/Authentication.tsx
@@ -2,12 +2,11 @@ import React, { FC } from "react";
 import { Row, Button } from "antd";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { SupabaseClient } from "@supabase/supabase-js";
-import { signIn, signInWithFacebook } from "../utils/singIn";
 import { useSession } from "@supabase/auth-helpers-react";
 
 interface AuthenticationProps {
   supabaseClient: SupabaseClient;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const Authentication: FC<AuthenticationProps> = (props) => {
@@ -16,15 +15,6 @@ const Authentication: FC<AuthenticationProps> = (props) => {
   if (!session) {
     return (
       <div style={{ padding: "5rem 5rem" }}>
-        <Row justify="center">
-          <Button onClick={() => signIn(supabaseClient)}>
-            Sign in with google
-          </Button>
-          <Button onClick={() => signInWithFacebook(supabaseClient)}>
-            Sign in with facebook
-          </Button>
-        </Row>
-
         <Auth
           supabaseClient={supabaseClient}
           appearance={{ theme: ThemeSupa }}
