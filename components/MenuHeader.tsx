a9d864fc4483f1db075ddb2ca210f79b7a5c96d5
Michael <michael@MacBook-Pro-tilhrende-Michael.local>
2022-10-27 22:08:45 +0200

    add navigation header

---
+++ components/MenuHeader.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import { Menu, Layout, Row, Col } from "antd";
+import type { MenuProps } from "antd";
+import { SupabaseClient, useSession } from "@supabase/auth-helpers-react";
+import signOut from "../utils/signOut";
+import Link from "next/link";
+
+const { Header } = Layout;
+
+interface MenuHeaderProps {
+  supabaseClient: SupabaseClient;
+}
+
+const MenuHeader: FC<MenuHeaderProps> = (props) => {
+  const { supabaseClient } = props;
+  const session = useSession();
+  const logout = <div onClick={() => signOut(supabaseClient)}>Log Out</div>;
+  const login = <Link href={"/login"}>Log In</Link>;
+  const signedInItems: MenuProps["items"] = [
+    {
+      label: <Link href={"/"}>Pool The Party</Link>,
+      key: "frontpage",
+    },
+    {
+      label: <Link href={"my-parties"}>My parties</Link>,
+      key: "parties",
+    },
+    {
+      label: <Link href={"create-party"}>Create party</Link>,
+      key: "createparty",
+    },
+    { label: logout, key: "logout" },
+  ];
+  const signedOutItems: MenuProps["items"] = [
+    {
+      label: <Link href={"/"}>Pool The Party</Link>,
+      key: "frontpage",
+    },
+    { label: login, key: "login" },
+  ];
+  return (
+    <Layout>
+      <Header style={{ background: "#fff" }}>
+        <Menu
+          items={session ? signedInItems : signedOutItems}
+          mode={"horizontal"}
+        />
+      </Header>
+    </Layout>
+  );
+};
+
+export default MenuHeader;
