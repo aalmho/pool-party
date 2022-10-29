333ec53065138d5f184e6986bab991f704b0d320
Michael <michael@MacBook-Pro-tilhrende-Michael.local>
2022-10-29 18:07:46 +0200

    refactor auth

--- components/Authentication.tsx
+++ components/Authentication.tsx
@@ -1,29 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { Row, Button } from "antd";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { useSession } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/router";
 
 interface AuthenticationProps {
   supabaseClient: SupabaseClient;
-  children?: React.ReactNode;
 }
 
-const Authentication: FC<AuthenticationProps> = (props) => {
-  const { supabaseClient, children } = props;
+const Authentication: FC<AuthenticationProps> = ({ supabaseClient }) => {
   const session = useSession();
-  if (!session) {
-    return (
-      <div style={{ padding: "5rem 5rem" }}>
-        <Auth
-          supabaseClient={supabaseClient}
-          appearance={{ theme: ThemeSupa }}
-          providers={["google", "facebook"]}
-        />
-      </div>
-    );
-  }
-  return <>{children}</>;
+  const router = useRouter();
+  useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  });
+  return (
+    <div style={{ padding: "5rem 5rem" }}>
+      <Auth
+        supabaseClient={supabaseClient}
+        appearance={{ theme: ThemeSupa }}
+        providers={["google", "facebook"]}
+      />
+    </div>
+  );
 };
 
 export default Authentication;
