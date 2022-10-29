c12bcb424849ab211fc855a9725109a8ebf77e2c
Michael <michael@MacBook-Pro-tilhrende-Michael.local>
2022-10-29 16:26:30 +0200

    public and private pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,30 @@ import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 import Authentication from "../components/Authentication";
-import { Button } from "antd";
+import { useRouter } from "next/router";
 import MenuHeader from "../components/MenuHeader";
+
+const publicPages = ["/", "/party/[id]"];
 
 function MyApp({
   Component,
   pageProps,
 }: AppProps<{ initialSession: Session }>) {
+  const router = useRouter();
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
+  const isPublicPage = publicPages.includes(router.pathname);
+
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
-      <Authentication supabaseClient={supabaseClient}>
-        <MenuHeader supabaseClient={supabaseClient} />
+      <MenuHeader supabaseClient={supabaseClient} />
+      {isPublicPage ? (
         <Component {...pageProps} />
-      </Authentication>
+      ) : (
+        <Authentication supabaseClient={supabaseClient}>
+          <Component {...pageProps} />
+        </Authentication>
+      )}
     </SessionContextProvider>
   );
 }
